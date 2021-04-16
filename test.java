b4a599cd8e315c2d1a98b1b9c1932a88d45401fc
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-04-16 13:56:08 +0200

    more changes

--- test.java
+++ test.java
@@ -4,12 +4,13 @@ public class Test {
 	public void TestMethod()
 	{
 		
-		int b = TestMethod2(1, "test");
+		TestMethod3(true, (float)0.2);
 	}
 	
-	private int TestMethod2(int a, String b)
+	
+	private void TestMethod3(boolean test, float test2)
 	{
 		
-		 
+		
 	}
 }
