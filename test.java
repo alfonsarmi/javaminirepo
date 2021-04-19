cc4502f1b15247a40b885c532c2a9f1d8be18c83
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-04-19 13:58:51 +0200

    dfdsaf

--- test.java
+++ test.java
@@ -1,40 +1,31 @@
 package net.code.java;
-
 class Tipo
 {
-	int test;
+	int test, requetetest;
 	int TestMethod4(int a, String b)
 	{
-		return 0;
-		
-	}
-	
+		return 0;		
+	}	
 	void TestMethod5(int a, String b)
 	{
 		int v = TestMethod4(1, "");		
-	}
-	
+	}	
 	static Tipo GetNew()
-	{
-		
+	{		
 		return new Tipo();
-	}
-	
+	}	
 	Tipo()
 	{
 		int v = TestMethod4(1, "");		
-	}
-	
+	}	
 	Tipo(int test)
 	{
 		int v = TestMethod4(1, "");		
 	}
 }
-
 public class Test {
 	public void TestMethod()
-	{
-		
+	{		
 		TestMethod3(true, (float)0.2);
 		Tipo nuevoTipo = Tipo.GetNew();
 		nuevoTipo.test = 0;
@@ -43,11 +34,7 @@ public class Test {
 		nuevoTipo.TestMethod4(a, "culo");
 		nuevoTipo.TestMethod5(a, "pis");	
 	}
-	
-	
 	private void TestMethod3(boolean test, float test2)
 	{
-		
-		
 	}
 }
