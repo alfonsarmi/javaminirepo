b6f00d4915cf1faca6c639bfa208954d84a0c7a9
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-04-19 09:47:04 +0200

    test

--- test.java
+++ test.java
@@ -1,17 +1,37 @@
 package net.code.java;
 
+class Tipo
+{
+	int test;
+	int TestMethod4(int a, String b)
+	{
+		return 0;
+		
+	}
+	
+	void TestMethod5(int a, String b)
+	{
+		int v = TestMethod4(1, "");		
+	}
+	
+	static Tipo GetNew()
+	{
+		
+		return new Tipo();
+	}
+}
+
 public class Test {
 	public void TestMethod()
 	{
 		
-		int b = TestMethod2(1, "test");
 		TestMethod3(true, (float)0.2);
-	}
-	
-	private int TestMethod2(int a, String b)
-	{
-		
-		 
+		Tipo nuevoTipo = Tipo.GetNew();
+		nuevoTipo.test = 0;
+		int a = nuevoTipo.TestMethod4(0, null);
+		int b = nuevoTipo.TestMethod4(0, "caca");
+		nuevoTipo.TestMethod4(a, "culo");
+		nuevoTipo.TestMethod5(a, "pis");	
 	}
 	
 	
